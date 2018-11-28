9ba92d8e6cb75cee378de6abe2a532283d22d691
gaoyadi <984831010@qq.com>
2018-11-28 10:40:55 +0800

    模态框完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -7,8 +7,37 @@ $(document).ajaxStop(function() {
   })
 })
 
+
 $(function(){
+  // 二级栏
   $('.category').click(function(){
     $(this).next().slideToggle();
+  });
+
+  $('.icon_left').click(function(){
+    $('.aside').toggleClass('hidemenu');
+    $('.topbar').toggleClass('hidemenu');
+    $('.main').toggleClass('hidemenu');
   })
+
+  // 模态框
+  $('.icon_right').click(function(){
+    $('#logoutModal').modal('show');
+  })
+  // 退出功能
+  $('#logoutBtn').click(function(){
+    $.ajax({
+      type: 'get',
+      url: '/employee/employeeLogout',
+      dataType: 'json',
+      success: function(info){
+        console.log(info);
+        if(info.success) {
+          location.href = 'login.html';
+        }
+      }
+    })
+  })
+  
 })
+
